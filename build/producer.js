6b514c8168ff99bdda814fcfa324e85063d771fe
Olasunkanmi Raymond <60177090+raymond-frontend@users.noreply.github.com>
2020-12-16 15:30:43 +0800

    create the kafka producer (#1)

---
+++ build/producer.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.publish = void 0;
+var kafka_node_1 = require("kafka-node");
+var kafkaHost = "localhost:9092";
+exports.publish = function (topic, message) {
+    //The client connects to Kafka
+    var client = new kafka_node_1.KafkaClient({ kafkaHost: kafkaHost });
+    //The producer writes to a topic
+    var producer = new kafka_node_1.Producer(client);
+    //Initiate the producer
+    producer.on("ready", function () {
+        client.refreshMetadata([topic], function (err) {
+            if (err) {
+                throw err;
+            }
+            console.log("Sending message to topic " + topic + ": " + message);
+            producer.send([{ topic: topic, messages: [message] }], function (err, result) {
+                console.log(err || result);
+                process.exit();
+            });
+        });
+    });
+    producer.on("error", function (err) {
+        console.log("error", err);
+    });
+};
