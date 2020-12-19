6071a594d12f80d35c428a3752baa428b0f2df28
Raymond Oyinlola <olasunkanmi@Raymonds-MacBook-Pro.local>
2020-12-19 22:06:08 +0800

    create the posts endpoints

--- posts/src/index.ts
+++ posts/src/index.ts
@@ -1,6 +1,27 @@
-class Post {
-  constructor(public hello: string) {}
-  greet() {
-    console.log(this.hello);
-  }
+import express from "express";
+import { randomBytes } from "crypto";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+interface Post {
+  id: string;
+  title: string;
 }
+
+const posts: Post[] = [];
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+  if (title.length < 1) {
+    return res.status(400).send("post title is required");
+  }
+  let post = { id, title };
+  posts.push(post);
+});
